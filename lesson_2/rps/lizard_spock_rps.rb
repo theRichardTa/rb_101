7db2b078830aa2adf5fc66e0d11b7aa7d1b39ae3
Richard Ta <TheRichardTa@gmail.com>
2022-11-21 23:34:20 -0600

    finished bonus on lsrps

---
+++ lesson_2/rps/lizard_spock_rps.rb
@@ -0,0 +1,113 @@
+WINNING_MOVES = {
+  'rock' => ['lizard', 'scissors'],
+  'lizard' => ['spock', 'paper'],
+  'spock' => ['rock', 'scissors'],
+  'scissors' => ['lizard', 'paper'],
+  'paper' => ['rock', 'spock']
+}
+
+VALID_MOVES = %w(rock lizard spock scissors paper)
+
+VALID_MOVES_SHORT = %w(r p s s l)
+
+def prompt(msg)
+  puts ">> #{msg}"
+end
+
+def error_prompt(msg)
+  puts ">>ERORR>> #{msg}"
+end
+
+def validate_choice(choice)
+  VALID_MOVES.include?(choice)
+end
+
+def validate_choice_short(choice)
+  VALID_MOVES_SHORT.include?(choice)
+end
+
+def winner?(player1, player2)
+  WINNING_MOVES[player1].include?(player2)
+end
+
+def s_clarify_choice
+  loop do
+    error_prompt "Did you mean to pick 1) Scissors or 2) Spock ?"
+    s_choice = gets.chomp
+    if s_choice == "1"
+      return "scissors"
+    elsif s_choice == "2"
+      return "spock"
+    else
+      error_prompt "Not a valid choice! (1 or 2)"
+    end
+  end
+end
+
+prompt "Welcome to RPSSL!"
+
+user_wins = 0
+computer_wins = 0
+
+loop do
+  user_choice = ''
+  loop do
+    prompt "Please choose either Rock, Paper, Scissors, Spock, or Lizard."
+    user_choice = gets.chomp.downcase
+    break if validate_choice(user_choice)
+
+    if validate_choice_short(user_choice)
+      case user_choice
+      when 's'
+        user_choice = s_clarify_choice
+      when 'r'
+        user_choice = 'rock'
+      when 'p'
+        user_choice = 'paper'
+      when 'l'
+        user_choice = 'lizard'
+      end
+      break
+    end
+
+    # I can use when / then here... the case is already defined FOR the value of
+    # user_choice.. try it
+
+    error_prompt "Not a valid choice!"
+  end
+
+  prompt "You chose #{user_choice}."
+
+  computer_choice = VALID_MOVES.sample
+  prompt "Computer chose #{computer_choice}."
+
+  if winner?(user_choice, computer_choice)
+    prompt "You won!"
+    user_wins += 1
+  elsif winner?(computer_choice, user_choice)
+    prompt "You lost!"
+    computer_wins += 1
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "You have #{user_wins} wins and the computer has #{computer_wins} wins."
+  puts ""
+
+  next unless user_wins == 3 || computer_wins == 3
+
+  if user_wins == 3
+    prompt "Congratulations! You are the Grand Winner!"
+  elsif computer_wins == 3
+    prompt "Sorry! The computer is the Grand Winner!"
+  end
+
+  user_wins = 0
+  computer_wins = 0
+
+  prompt "Would you like to play again?"
+  again_answer = gets.chomp.downcase
+  break unless again_answer == "y"
+end
+
+prompt "Thanks for playing. Good bye!"
