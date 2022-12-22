e6463695a0681cb23c447c9a50b1f20eb5ddaaab
Richard Ta <TheRichardTa@gmail.com>
2022-12-21 23:58:58 -0600

    finished easy_7

--- practice.rb
+++ practice.rb
@@ -1,12 +1,3 @@
-a = [2, 4]
+poo = ""
 
-b = [0, 10]
-
-
-yo = a.product(b).map do |num1, num2, num3|
-  num1 + num2
-end
-
-
-
-p yo.sort
+p poo.split
---
+++ small_problems/easy_7/ex10.rb
@@ -0,0 +1,17 @@
+def penultimate(string)
+  string.split[-2]
+end
+
+p penultimate('Launch School is great!') == 'is'
+p penultimate('last word') == 'last'
+
+# bonus question
+
+def mid_word(string)
+  middle_index = (string.split.size / 2.0).round - 1 # redundancy here with .float and .round
+  string.split[middle_index]
+end
+
+p mid_word("") # empty case nils but it didn't error or give wrong answer
+
+# seems right
