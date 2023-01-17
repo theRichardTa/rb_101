b461248e84c69da26203df11c4c0766a50535ab6
Richard Ta <TheRichardTa@gmail.com>
2023-01-16 23:18:34 -0800

    testing more merge_sort

--- practice.rb
+++ practice.rb
@@ -1,1 +1,44 @@
-finished video 1
+def merge_sort(array)
+  n = array.length
+  return array if n <= 1
+  sub_size = 1
+  while sub_size < n
+      i = 0
+      while i < n - sub_size
+          merge(array, i, i+sub_size-1, [i+sub_size*2-1, n-1].min)
+          i += sub_size*2
+      end
+      sub_size *= 2
+  end
+  return array
+end
+
+def merge(array, start, middle, ending)
+  left_part = array[start..middle]
+  right_part = array[(middle+1)..ending]
+  i = 0
+  j = 0
+  k = start
+  while i < left_part.size && j < right_part.size
+      if left_part[i] <= right_part[j]
+          array[k] = left_part[i]
+          i += 1
+      else
+          array[k] = right_part[j]
+          j += 1
+      end
+      k += 1
+  end
+  while i < left_part.size
+      array[k] = left_part[i]
+      i += 1
+      k += 1
+  end
+  while j < right_part.size
+      array[k] = right_part[j]
+      j += 1
+      k += 1
+  end
+end
+
+p merge_sort([5,2,1])
